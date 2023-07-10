a9907f3611eaaecfeae32cdea282a9b013e2bac9
Seung Jun <jsznawa@gmail.com>
2023-07-11 06:45:09 +0900

    0711 전승준 2문제

---
+++ 전승준/Week17/BOJ_1446_지름길.java
@@ -0,0 +1,21 @@
+package Week17;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1446_지름길 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+		int[]  broad = new int
+		
+		for(int i=0; i<N; ++i) {
+			
+		}
+	}
+
+}
