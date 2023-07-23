1f5e151df667eae4d5e9f90ed488cc289a514a6f
chasonghui <chasonghui@naver.com>
2023-07-23 19:14:09 +0900

    week19-bj17609

---
+++ 차송희/week19/bj17609.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ17609 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            String input = br.readLine();
+            int result = isPalindrome(input);
+            System.out.println(result);
+        }
+    }
+
+    private static int isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                if (isRange(str, left + 1, right) || isRange(str, left, right - 1)) {
+                    return 1;
+                } else {
+                    return 2;
+                }
+            }
+
+            left++;
+            right--;
+        }
+
+        return 0;
+    }
+
+    private static boolean isRange(String str, int left, int right) {
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
+
